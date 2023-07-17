e9b20868b4571434ab53d7b835f140e558a7f8cc
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-17 10:37:38 +0200

    controlling zoomins and zoomouts - exercise 289

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -9,18 +9,48 @@ export class ZoomRangePageComponent implements AfterViewInit {
 
   @ViewChild('map') divMap?: ElementRef;
 
+  public currentZoom: number = 10;
+  public map?: Map;
 
   ngAfterViewInit(): void {
 
     if ( !this.divMap ) throw 'This HTML file was not found';
     
     
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.currentZoom, // starting zoom
     });
+
+    this.mapListeners();
+  }
+
+  mapListeners() {
+    if ( !this.map ) throw 'Map not found';
+
+    this.map.on('zoom', (ev) => {
+      this.currentZoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => { //listener para que cuando hagamos demasiado zoom vuelva a valor zoom 18
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn(); //para botón zoom in
+  }
+
+  zoomOut() {
+    this.map?.zoomOut(); //para botón zoom out
+  }
+
+  zoomChanged( value: string ) {
+    this.currentZoom = Number(value);
+    this.map?.zoomTo( this.currentZoom );
   }
   
 }
