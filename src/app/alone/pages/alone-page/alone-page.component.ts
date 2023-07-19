4b6534c91ddf358f8eb2a4270b7685df514ac5d9
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-19 12:06:38 +0200

    standalone as reusable + nested (optional) component - exercise 304

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   standalone: true,
-/*   imports: [CommonModule],
- */  templateUrl: './alone-page.component.html',
+  imports: [CounterAloneComponent],
+  templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css']
 })
 export class AlonePageComponent {
