a53b870f194eb490f1bee6cec1c3c9644a3bffbf
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-19 13:31:20 +0200

    converted traditional component into a standalone one + finished section - exercise 306

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -7,7 +7,6 @@ import * as mapboxgl from 'mapbox-gl';
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
@@ -15,22 +14,23 @@ import { PropertiesPageComponent } from './pages/properties-page/properties-page
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 
 import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
-    MiniMapComponent,
-    SideMenuComponent,
-    MapsLayoutComponent,
     FullScreenPageComponent,
+    MapsLayoutComponent,
     MarkersPageComponent,
+    MiniMapComponent,
     PropertiesPageComponent,
     ZoomRangePageComponent,
   ],
   imports: [
     CommonModule,
+    CounterAloneComponent,
     MapsRoutingModule,
-    CounterAloneComponent
+    SideMenuComponent
   ]
 })
 export class MapsModule { }
